cfd0d7ca780376334f170c539f0acabc7cfc5a70
JackQuicker1 <114510536+JackQuicker1@users.noreply.github.com>
2024-06-07 13:22:17 -0400

    Update JackBot.js
    ChatGPT version

--- JackBot.js
+++ JackBot.js
@@ -18,28 +18,78 @@ class JackBot{
                                 /\
                         Player  ||
   */
-  getMove(wells, store1, store2){
-    let move = 2;
-    let directMove = false;
-    for(let i=5; i>=0; i--){
-      if(wells[i] == 6-i){
-        move = i;
-        directMove = true;
-       break;
-      }
-    }
-    if(directMove == false){
-      for(let i=5; i>=0; i--){
-        if(wells[i] >=1){
-          move = i;
-          break;
-        }
-      }
-    }
+  // getMove(wells, store1, store2){
+  //   let move;
+  //   let directMove = false;
+  //   for(let i=5; i>=0; i--){
+  //     if(wells[i] == 6-i){
+  //       move = i;
+  //       directMove = true;
+  //      break;
+  //     }
+  //   }
+  //   //picks after perfect move
+  //   if(directMove == false){
+  //     for(let i=5; i>=0; i--){
+  //       if(wells[i] >=1){
+  //         move = i;
+  //         break;
+  //       }
+  //     }
+  //   }
         
     
+  //   return move;
+  // }
+  function getMove(wells, store1, store2){
+    let move = -1;
+    let directMove = false;
+    
+    // Perfect move: if the number of beads in the well equals the index of the well plus 1
+    for(let i = 5; i >= 0; i--){
+        if(wells[i] === 6 - i){
+            move = i;
+            directMove = true;
+            break;
+        }
+    }
+
+    // Check for captures: if a move ends in an empty well on your side
+    if(!directMove){
+        for(let i = 5; i >= 0; i--){
+            let endIndex = (i + wells[i]) % 12;
+            if(wells[i] > 0 && endIndex < 6 && wells[endIndex] === 0){
+                move = i;
+                directMove = true;
+                break;
+            }
+        }
+    }
+
+    // Avoid moves that give the opponent a turn
+    if(!directMove){
+        for(let i = 5; i >= 0; i--){
+            let endIndex = (i + wells[i]) % 12;
+            if(wells[i] > 0 && endIndex !== 6){
+                move = i;
+                directMove = true;
+                break;
+            }
+        }
+    }
+
+    // If no direct move, pick the first available move
+    if(!directMove){
+        for(let i = 5; i >= 0; i--){
+            if(wells[i] >= 1){
+                move = i;
+                break;
+            }
+        }
+    }
+    
     return move;
-  }
+}
  
   getName(){
     return this.name
